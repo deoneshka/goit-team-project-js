889c99fc859ba50896897f58ec61c2ad8fbbf3e7
Yuliia Dvoinos <69351064+YuliiaDvoinos@users.noreply.github.com>
2021-03-30 20:08:53 +0300

    Update index.js

--- src/js/setting/index.js
+++ src/js/setting/index.js
@@ -1,6 +1,6 @@
 const options = {
     API_KEY: 'ed4770d472da6341d2e53ccb9e288320',
-    BASE_URL: 'http://api.themoviedb.org/3/',
-    IMG_URL: 'http://image.tmdb.org/t/p/w500/'
+    BASE_URL: 'https://api.themoviedb.org/3/',
+    IMG_URL: 'https://image.tmdb.org/t/p/w500/'
 }
 export default options;
